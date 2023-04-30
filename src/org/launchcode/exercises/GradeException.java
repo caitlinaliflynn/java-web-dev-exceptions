c808dbfb162e28db2545d7b3c1cbae71a4209646
Ali <caitlinaliflynn@gmail.com>
2023-04-29 19:43:27 -0500

    finished part of exceptions exercise

---
+++ src/org/launchcode/exercises/GradeException.java
@@ -0,0 +1,8 @@
+//package org.launchcode.exercises;
+//
+//public class GradeException extends Main {
+//
+//    public GradeException(String message) {
+//        super(message);
+//    }
+//}
--- src/org/launchcode/exercises/Main.java
+++ src/org/launchcode/exercises/Main.java
@@ -1,13 +1,28 @@
 package org.launchcode.exercises;
 
+import org.launchcode.Temperature.TemperatureException;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
         // Test out your Divide() function!
+        Scanner input;
+
+        input = new Scanner(System.in);
+        System.out.println("Enter a value for x: ");
+        int x = input.nextInt();
+
+        System.out.println("Enter a value for y: ");
+        int y = input.nextInt();
+        input.close();
+
+        Divide(x, y);
+
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -16,16 +31,45 @@ public class Main {
         studentFiles.put("Stefanie", "CoolProgram.java");
 
         // Test out your CheckFileExtension() function!
+
+        CheckFileExtension("Program.java");
     }
 
-    public static void Divide(int x, int y)
-    {
+    public static int Divide(int x, int y) {
         // Write code here!
+        if (y == 0) {
+            try {
+                throw new ArithmeticException("Y cannot equal zero.");
+            } catch (ArithmeticException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return x/y;
+
     }
 
-    public static int CheckFileExtension(String fileName)
-    {
-        // Write code here!
+    public static int CheckFileExtension(String fileName) {
+
+        int point = 0;
+
+        if (fileName.contains(".java")) {
+            point = 1;
+            return point;
+        }
+        if (!fileName.contains(".java")) {
+            point = 0;
+            return point;
+        }
+        if (fileName == null) {
+            try {
+                throw new ArithmeticException("File submitted is null or an empty string");
+            } catch (ArithmeticException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return point;
     }
 
 }
